8f916d69c450c72471fa0f814df2eb8ebe068816
Mariusz Kopylec <mariusz.kopylec@allegrogroup.com>
2015-11-25 16:10:22 +0100

    try

--- recaptcha-spring-security-sample/src/main/java/com/github/mkopylec/recaptcha/SecurityConfiguration.java
+++ recaptcha-spring-security-sample/src/main/java/com/github/mkopylec/recaptcha/SecurityConfiguration.java
@@ -1,33 +1,24 @@
 package com.github.mkopylec.recaptcha;
 
-import com.github.mkopylec.recaptcha.security.RecaptchaAuthenticationFilter;
-import com.github.mkopylec.recaptcha.security.login.CredentialLoginFailuresCountingHandler;
-import com.github.mkopylec.recaptcha.security.login.LoginFailuresClearingHandler;
+import com.github.mkopylec.recaptcha.security.login.FormLoginConfigurerEnhancer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private RecaptchaAuthenticationFilter authenticationFilter;
-    @Autowired
-    private CredentialLoginFailuresCountingHandler failureHandler;
-    @Autowired
-    private LoginFailuresClearingHandler successHandler;
+    private FormLoginConfigurerEnhancer enhancer;
 
     @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .formLogin().loginPage("/login").failureHandler(failureHandler).successHandler(successHandler)
+    protected void configure(HttpSecurity http) throws Exception {
+        enhancer.addRecaptchaSupport(http.formLogin()).loginPage("/login")
                 .and()
-                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable()
                 .authorizeRequests().antMatchers("/").authenticated();
     }
 }
